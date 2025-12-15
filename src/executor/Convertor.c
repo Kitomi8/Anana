86745a7fddee55eb2f38b4e832c1c186dd396f92
kitomi_m <114172628+Kitomi8@users.noreply.github.com>
2025-12-15 09:01:51 +0300

    Create Convertor.c

---
+++ src/executor/Convertor.c
@@ -0,0 +1,40 @@
+#include "minishell.h"
+
+int	count_token_list(t_token *tokens)
+{
+	int		count;
+	t_token	*tmp;
+
+	count = 0;
+	tmp = tokens;
+	while (tmp)
+	{
+		count++;
+		tmp = tmp->next;
+	}
+	return (count);
+}
+
+
+char	**token_to_tab(t_token *tokens)
+{
+	char	**args;
+	int		count;
+	int		i;
+	t_token	*tmp;
+
+	count = count_token_list(tokens);
+	args = malloc(sizeof(char *) * (count + 1));
+	if (!args)
+		return (NULL);
+	tmp = tokens;
+	i = 0;
+	while (tmp)
+	{
+		args[i] = ft_strdup(tmp->content);
+		i++;
+		tmp = tmp->next;
+	}
+	args[i] = NULL; 
+	return (args);
+}
